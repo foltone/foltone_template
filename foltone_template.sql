e8e478f7b6c216c6fba6efbfd8f5dd1d74e2c2a4
Foltone <aug.viel@gmail.com>
2022-08-02 21:33:54 +0200

    update weight SQL

--- foltone_template.sql
+++ foltone_template.sql
@@ -107,9 +107,9 @@ CREATE TABLE `items` (
 --
 
 INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
-('eau', 'Bouteille d\'eau', 1, 3, 1),
+('eau', 'Bouteille d\'eau', 0.2, 3, 1),
 ('clip', 'Chargeur', 1, 3, 1),
-('pain', 'Pain', 1, 3, 1);
+('pain', 'Pain', 0.2, 3, 1);
 
 -- --------------------------------------------------------
 
